e0e743698ea4b2d133bec9f8d8ea7137470bfed2
arbaz123-arch <arbazdinnagar123@gmail.com>
2025-11-14 02:00:45 +0530

    Initial commit: Added config folder where i did some cofiguration settings

---
+++ config/settings.py
@@ -0,0 +1,15 @@
+# ========================
+# CONFIG
+# ========================
+
+DEFAULT_MESSAGE_TEMPLATE = (
+    "Hi {name},\n\n"
+    "hii, this arbaz khan from jasiq labs pvt. ptd.\n"
+)
+
+SLEEP_BETWEEN_MESSAGES_SEC = (4, 8)
+BATCH_SIZE_BEFORE_BREAK = 60
+BATCH_BREAK_SEC = 180
+DRY_RUN = False
+START_FROM_INDEX = 0
+MAX_TO_SEND = None  # None = all
